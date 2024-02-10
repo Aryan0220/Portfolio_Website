9594e973da768a411dd1391d4f8591934610afad
Aryan <aryan.gorde4@gmail.com>
2024-02-10 19:45:55 +0530

    Create Navbar

---
+++ ui/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <section id="project">project</section>
+      <section id="skills">skills</section>
+      <section id="about">about</section>
+      <section id="contact">contact</section>
+    </div>
+  );
+};
+
+export default App;
