7169161466ba211215fc2d5cb48346f26f130965
Aryan <aryan.gorde4@gmail.com>
2024-02-14 21:24:48 +0530

    Create NavBar with Tailwind

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -3,23 +3,46 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="nav-bar">
-      <nav>
-        <div className="logo">Aryan Bargal</div>
-        <ul className="nav-links">
+      <nav className="flex flex-row justify-around bg-transparent items-center">
+        <div className="text-4xl">Aryan Bargal</div>
+        <ul className="flex flex-row gap-8 list-none ml-8">
           <li>
-            <a href="#project">PROJECTS</a>
+            <a
+              href="#project"
+              className="text-2xl  no-underline hover:text-indigo-500 hover:underline-offset-0.5 hover:underline hover:decoration-slate-200"
+            >
+              PROJECTS
+            </a>
           </li>
           <li>
-            <a href="#skill">SKILLS</a>
+            <a
+              href="#skill"
+              className="text-2xl  no-underline hover:text-indigo-500 hover:underline-offset-0.5 hover:underline hover:decoration-slate-200"
+            >
+              SKILLS
+            </a>
           </li>
           <li>
-            <a href="#about">ABOUT</a>
+            <a
+              href="#about"
+              className="hover:underline-offset-0.5 text-2xl  no-underline hover:text-indigo-500  hover:underline hover:decoration-slate-200"
+            >
+              ABOUT
+            </a>
           </li>
           <li>
-            <a href="#contact">CONTACT</a>
+            <a
+              href="#contact"
+              className="text-2xl  no-underline hover:text-indigo-500 hover:underline-offset-0.5 hover:underline hover:decoration-slate-200"
+            >
+              CONTACT
+            </a>
           </li>
         </ul>
-        <button className="resume" type="button">
+        <button
+          className="resume text-resume border-3 border-indigo-500 bg-indigo-500 rounded-full px-3 py-1 hover:bg-transparent hover:border-slate-200"
+          type="button"
+        >
           Resume
         </button>
       </nav>
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
   content: ["./src/**/*.{js,jsx}", "./public/*.html"],
   theme: {
-    fontFamily: {
-      body: ["Poppins"],
+    extend: {
+      fontFamily: {
+        body: ["Poppins"],
+      },
+      textUnderlineOffset: {
+        0.5: "0.5rem",
+      },
+      fontSize: {
+        "2xl": "1.4rem",
+        "4xl": "2.2rem",
+        resume: "1.05rem",
+      },
+      borderWidth: {
+        DEFAULT: "1px",
+        3: "3px",
+      },
+      padding: {
+        "8px": "8px",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
